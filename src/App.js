628f50b1a1f109a22b4cfbaa329ee8bc657b6474
balkac <furkanbalkac@gmail.com>
2024-07-16 11:56:28 +0300

    ContactList added to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { getContacts } from "./api/ContactService";
 import Header from "./components/Header";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import ContactList from "./components/ContactList";
 function App() {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
@@ -10,9 +11,9 @@ function App() {
   const getAllContacts = async (page = 0, size = 10) => {
     try {
       setCurrentPage(page);
-      const response = await getContacts(page, size);
-      setData(response.data);
-      console.log(response.data);
+      const response = (await getContacts(page, size)).data;
+      setData(response);
+      console.log(response);
     } catch (error) {
       console.error("Error fetching data", error);
     }
@@ -26,7 +27,28 @@ function App() {
     getAllContacts();
   }, []);
 
-  return <Header toggleModal={toggleModal} numberofContacts={50} />;
+  return (
+    <>
+      <Header toggleModal={toggleModal} numberofContacts={data.totalElements} />
+      <main className="main">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Navigate to={"/contacts"} />} />
+            <Route
+              path="/contacts"
+              element={
+                <ContactList
+                  data={data}
+                  currentPage={currentPage}
+                  getAllContacts={getAllContacts}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Contact from "./Contact";
 
-const ContactList = (data, currentpage, getAllContacts) => {
+const ContactList = ({ data, currentPage, getAllContacts }) => {
   return (
     <main className="main">
       {data?.content?.length === 0 && (
@@ -9,20 +9,19 @@ const ContactList = (data, currentpage, getAllContacts) => {
       )}
 
       <ul className="contact__list">
-        {data?.content.length > 0 &&
+        {data?.content?.length > 0 &&
           data.content.map((contact) => (
             <Contact contact={contact} key={contact.id} />
           ))}
-        s
       </ul>
 
       {data?.content?.length > 0 && data?.totalPages > 1 && (
         <div className="pagination">
           <a
-            onClick={() => getAllContacts(currentpage - 1)}
-            className={0 === currentpage ? "disabled" : ""}
+            onClick={() => getAllContacts(currentPage - 1)}
+            className={0 === currentPage ? "disabled" : ""}
           >
-            Previous Page
+            &laquo;
           </a>
 
           {data &&
@@ -37,10 +36,10 @@ const ContactList = (data, currentpage, getAllContacts) => {
             ))}
 
           <a
-            onClick={() => getAllContacts(currentpage + 1)}
-            className={data.totalPages === currentpage + 1 ? "disabled" : ""}
+            onClick={() => getAllContacts(currentPage + 1)}
+            className={data.totalPages === currentPage + 1 ? "disabled" : ""}
           >
-            Next Page
+            &raquo;
           </a>
         </div>
       )}
